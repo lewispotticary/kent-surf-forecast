69712d0cd0c99095a321cb56726a23c28dd06df3
Lewis Potticary <lewispotticary@gmail.com>
2021-03-20 15:14:10 +0000

    Added more surf spots to index.html map

--- js/map.js
+++ js/map.js
@@ -4,11 +4,57 @@ var mymap = L.map('mapid').setView([51.35613, 1.40256], 12.4);
             attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         }).addTo(mymap);
 
-        var jossbay = L.marker([51.37965,1.44583]).addTo(mymap);
-        jossbay.bindPopup("Joss Bay");
-        jossbay.on('mouseover', function (e) {
+        //Joss Bay Marker
+
+        var jossBay = L.marker([51.37965,1.44583]).addTo(mymap);
+        jossBay.bindPopup("Joss Bay");
+        jossBay.on('mouseover', function (e) {
             this.openPopup();
         });
-        jossbay.on('mouseout', function (e) {
+        jossBay.on('mouseout', function (e) {
+            this.closePopup();
+        });
+
+        //Viking Bay Marker
+
+        var vikingBay = L.marker([51.35777, 1.44358]).addTo(mymap);
+        vikingBay.bindPopup("Viking Bay");
+        vikingBay.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        vikingBay.on('mouseout', function (e) {
+            this.closePopup();
+        });
+
+        //Margate Beach Marker
+
+        var margateBeach = L.marker([51.38803, 1.37679]).addTo(mymap);
+        margateBeach.bindPopup("Margate Beach");
+        margateBeach.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        margateBeach.on('mouseout', function (e) {
+            this.closePopup();
+        });
+
+        //Ramsgate Main Sands Marker
+
+        var ramsgateBeach = L.marker([51.33145, 1.4249]).addTo(mymap);
+        ramsgateBeach.bindPopup("Ramsgate Main Sands");
+        ramsgateBeach.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        ramsgateBeach.on('mouseout', function (e) {
+            this.closePopup();
+        });
+
+        //Western Undercliff Beach Marker
+
+        var westernUndercliff = L.marker([51.3265, 1.40709]).addTo(mymap);
+        westernUndercliff.bindPopup("Western Undercliff Beach");
+        westernUndercliff.on('mouseover', function (e) {
+            this.openPopup();
+        });
+        westernUndercliff.on('mouseout', function (e) {
             this.closePopup();
         });
